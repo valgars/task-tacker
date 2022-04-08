e681e85af276269b59c6482cb107f6d1e51b911e
valgars <alexandr.pylypchuk@gmail.com>
2022-04-08 22:22:36 +0300

    v2

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+const Button = ({color, text, onClick}) => {
+    return (
+        <button 
+            onClick={onClick}
+            className="btn" 
+            style={{backgroundColor: color}}>
+                {text}
+        </button>
+    )
+}
+
+Button.defaultProps= {
+    color: "steelblue"
+}
+
+export default Button
