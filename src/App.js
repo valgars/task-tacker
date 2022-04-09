646e1911a3c74610d5728433513bac7201ef2ff5
valgars <alexandr.pylypchuk@gmail.com>
2022-04-09 23:47:25 +0300

    v4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from "./components/AddTask";
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
       {
@@ -49,8 +50,10 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)}
+        showAdd = {showAddTask}
+        />
+      {showAddTask && <AddTask onAdd={addTask}/> }
       {tasks.length > 0 ?
         < Tasks
           tasks={tasks}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import Button from "./Button"
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({ title , onAdd, showAdd}) => {
     return (
         <header className="header">
             <h1>{title}</h1>
             <Button
-                color='green'
-                text='Add'
-                onClick={onClick} />
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
         </header>
     )
 }
