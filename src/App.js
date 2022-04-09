ebe52c86736aeab45060b545eec7a9c6cd8087f7
valgars <alexandr.pylypchuk@gmail.com>
2022-04-09 23:37:01 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 
 const App = () => {
@@ -27,20 +28,35 @@ const App = () => {
     ]
   )
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000)+1;
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
-
+  // Toogle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ?
+        { ...task, reminder: !task.reminder } :
+        task))
+  }
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? 
-        < Tasks 
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ?
+        < Tasks
           tasks={tasks}
           onDelete={deleteTask}
-          /> : 'No tasks'} 
+          onToggle={toggleReminder}
+        /> : 'No tasks'}
     </div>
   );
 }
